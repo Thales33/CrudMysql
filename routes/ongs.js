b1c4f274068c70fc84a03811ba808cc50411c8ed
Thales Santana <tpsthales@gmail.com>
2015-04-08 02:13:26 -0300

    CrudFinalizado

--- routes/ongs.js
+++ routes/ongs.js
@@ -50,12 +50,78 @@ router.post('/add',function(req, res){
     		console.log(err);
     		res.send('Erro ao adicionar Ong ao Banco de Dados')
     	} else{
-    		res.location('/ongs/onglist');
-   	        res.redirect('/ongs/onglist');
+    		
+   	        res.redirect('ongs/onglist');
    	        console.log('Adicionado ao Banco de Dados');
     	}
     });
    });
+
+
+/*  Get atributos para Editar Ong. */
+router.get('/editar/:id', function(req, res) {
+    var db = req.db;
+    var id = req.params.id;
+
+    db.query("SELECT * FROM multiform.ongs WHERE id = ?",[id],function(err, rows,field){
+               if (err) {
+               	console.log(err);
+               	               }
+               
+            res.render('ongs/editarOng', {
+            onglist : rows , title: 'Editar ONG'
+        });
+    });
+    
+});
+
+/* Editar Ong */
+
+router.post('/editarOng',function(req,res){
+	var db = req.db;
+
+	// Get valores do formulário
+	var id = req.body.id;
+	var set = {
+         nome : req.body.nome,
+        email : req.body.email,
+     telefone : req.body.telefone,
+       bairro : req.body.bairro
+}
+  db.query("UPDATE multiform.ongs SET ? WHERE id = ?",[set,id],function(err){
+
+    	if(err){
+    		console.log(err);
+    		res.send('Erro ao Editar Ong no Banco de Dados')
+    	} else{
+    		
+   	        res.redirect('/ongs/onglist');
+   	        console.log('Editado com sucesso!');
+    	}
+    });
+});
+
+/* Deletar Ong */
+
+router.get('/delete/:id',function(req,res){
+	var db = req.db;
+
+	// Get valores do formulário
+	var id = req.params.id;
+	
+  db.query("DELETE FROM multiform.ongs WHERE id = ?",[id],function(err){
+
+    	if(err){
+    		console.log(err);
+    		res.send('Erro ao Deletar Ong do Banco de Dados')
+    	} else{
+    		
+   	        res.redirect('/ongs/onglist');
+   	        console.log('Deletado com sucesso!');
+    	}
+    });
+});
+
 
 
 
